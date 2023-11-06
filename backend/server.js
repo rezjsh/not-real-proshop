a13b2c5aa1ea69654ebad3b3e391eb5c0fd51e31
rezjsh <149146755+rezjsh@users.noreply.github.com>
2023-11-06 08:11:03 +0330

    create mongodb connection using monoose

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-import express from 'express'
-import products from './data/products.js'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
+connectDB();
+const app = express();
 
-const app = express()
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+app.get("/api/products", (req, res) => {
+  console.log(req);
+  res.json(products);
+});
 
-app.get('/api/products', (req, res) => {
-    console.log(req)
-    res.json(products)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => {
+    return p._id === req.params.id;
+  });
+  res.json(product);
+});
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => {
-       return   p._id === req.params.id
-    })
-    res.json(product)
-})
-
-
-app.listen(port, ()=>console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
