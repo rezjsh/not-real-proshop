d1c63b8196324a0873130ddc07d5549bed7ad1e4
rezjsh <149146755+rezjsh@users.noreply.github.com>
2023-11-07 15:20:52 +0330

    add routes to productRoutes.js

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,17 @@ import products from "./data/products.js";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMidleware.js";
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  console.log(req);
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => {
-    return p._id === req.params.id;
-  });
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
